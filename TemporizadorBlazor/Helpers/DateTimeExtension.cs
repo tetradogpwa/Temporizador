0009c666182a29f169de75da77a478db3c9c70ba
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-05-09 18:19:45 +0200

    subir archivos

---
+++ TemporizadorBlazor/Helpers/DateTimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemporizadorBlazor.Helpers
+{
+    public static class DateTimeExtension
+    {
+        public static string HoursMinutesString(this DateTime date)
+        {
+            return date.Hour.ToString().PadLeft(2, '0') + ":" + date.Minute.ToString().PadLeft(2, '0');
+        }
+        public static int TotalMinutes(this DateTime date) => (date.Hour * 60) + date.Minute;
+        public static bool IsValid(this DateTime date) => !Equals(date, default(DateTime));
+    }
+}
