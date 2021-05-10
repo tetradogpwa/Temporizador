fd425a9dc5fbb0fb10a91e97c15f8e7579158d45
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-05-10 17:27:01 +0200

    desarrollando pwa

---
+++ wwwroot/ServiceWorker.js
@@ -0,0 +1,83 @@
+const baseURL = '/';
+const indexURL = '/index.html';
+const networkFetchEvent = 'fetch';
+const swInstallEvent = 'install';
+const swInstalledEvent = 'installed';
+const swActivateEvent = 'activate';
+const staticCachePrefix = 'blazor-cache-v';
+const staticCacheName = 'blazor-cache-v1';
+const requiredFiles = [
+"/404.html",
+"/css/app.css",
+"/css/bootstrap/bootstrap.min.css",
+"/css/bootstrap/bootstrap.min.css.map",
+"/css/open-iconic/FONT-LICENSE",
+"/css/open-iconic/font/css/open-iconic-bootstrap.min.css",
+"/css/open-iconic/font/fonts/open-iconic.eot",
+"/css/open-iconic/font/fonts/open-iconic.otf",
+"/css/open-iconic/font/fonts/open-iconic.svg",
+"/css/open-iconic/font/fonts/open-iconic.ttf",
+"/css/open-iconic/font/fonts/open-iconic.woff",
+"/css/open-iconic/ICON-LICENSE",
+"/css/open-iconic/README.md",
+"/Data/beep.mp3",
+"/default-icon-192x192.png",
+"/favicon.ico",
+"/icon-512.png",
+"/index.html",
+"/ServiceWorkerRegister.js",
+"/manifest.json"
+];
+// * listen for the install event and pre-cache anything in filesToCache * //
+self.addEventListener(swInstallEvent, event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(requiredFiles);
+            })
+    );
+});
+self.addEventListener(swActivateEvent, function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (staticCacheName !== cacheName && cacheName.startsWith(staticCachePrefix)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+self.addEventListener(networkFetchEvent, event => {
+    const requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === baseURL) {
+            event.respondWith(caches.match(indexURL));
+            return;
+        }
+    }
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.ok) {
+                            if (requestUrl.origin === location.origin) {
+                                caches.open(staticCacheName).then(cache => {
+                                    cache.put(event.request.url, response);
+                                });
+                            }
+                        }
+                        return response.clone();
+                    });
+            }).catch(error => {
+                console.error(error);
+            })
+    );
+});
