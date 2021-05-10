f75bc0cda3587269333105286e5b5c9b797a0df4
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-05-10 17:00:36 +0200

    commit

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -1,14 +1,16 @@
 const VERSION = "1.0";
 const APP = "TemporizadoresBlazor";
 const CACHE_NAME = "CACHE_" + APP;
+
 self.addEventListener('fetch', e => {
 
     const respuesta=caches.match(e.request).then(resp => {
         if (!resp) {
             //no existe la busco por internet
             resp = fetch(e.request).then(respuesta => {
-                caches.open(CACHE_NAME).then(cache => cache.put(e.request, respuesta));
-                return respuesta.clone();
+                var respCache = respuesta.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(e.request, respCache));
+                return respuesta;
             });
         }
         return resp;
